0d0128edfaac63718ba510a5201f2d674bb35dd6
gallagsf <sfgal1m@gmail.com>
2019-09-21 07:07:19 -0400

    Created Person and PersonGenerator
    Person: created constructor, getters and setters, functions required in prompt, validation
    
    PersonGenerator: accepts user input while prompting for new users, writes generated people into a text file

--- src/PersonReader.java
+++ src/PersonReader.java
@@ -1,6 +1,10 @@
 public class PersonReader {
     public static void main(String[] args) {
-        // Write your code here ... ⬇
+        //
+        //    Use the JFileChooser to prompt the user to select the file you export in Part 1.
+        //    Instantiate a Person Object for each record you read from the file.
+        //    Store all the Person Objects in an ArrayList<Person>.
+        //    Use the String.format or System.out.printf method to create a neatly formatted columnar display of the data.
 
     }
 }
